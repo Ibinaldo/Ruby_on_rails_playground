459bfd3d788ea63b11f79d624154d94a8e014ec3
Ibinabo Bestmann <ibi.best@gmail.com>
2021-06-23 03:42:30 +0000

    Generated pages controller using command

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+  get 'pages/contact'
+  #get 'hello', to: 'pages/contact' # Bascially if you go to your site_url/hello, it redirects you to contact
+  get 'pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
